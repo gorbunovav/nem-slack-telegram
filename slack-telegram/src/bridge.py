0e68f786f807999a2491d32f48dbc50036453e63
Andrey Gorbunov <andrey@optimuspro.ru>
2017-02-19 01:10:48 +0300

    Missing files from the previous commit

--- slack-telegram/src/bridge.py
+++ slack-telegram/src/bridge.py
@@ -8,33 +8,27 @@ import threading
 import time
 import ConfigParser
 import logging
+import json
 from slack_coms import SlackManager
 from telegram_coms import TelegramManager
-
-logging.basicConfig(filename='bridge.log',
-                    format='%(levelname)s: %(asctime)s %(message)s in %(module)s on line %(lineno)d',
-                    level=logging.ERROR)
-
-#logging.getLogger().addHandler(logging.StreamHandler())
 
 Config = ConfigParser.ConfigParser()
 Config.read("config.ini")
 
+logging.basicConfig(format='%(levelname)s: %(asctime)s %(message)s in %(module)s on line %(lineno)d',
+                    level=Config.get('Logger', 'Level'),
+                    handlers=[logging.StreamHandler()
+                              #,logging.FileHandler("bridge.log")
+                             ]
+                   )
+
 SLACK_TOKEN = Config.get('Token', 'Slack')
 TELEGRAM_TOKEN = Config.get('Token', 'Telegram')
 
-
-SLACK_CHANNEL_MATCHING = {'G0BCJ6A11': -23053030,
-                             'G085E7UF2': -14284494,
-                             'G0C7PQQ5V': -11209025,
-                             'G0K754RGB': -108453376,
-                             'G0K7MCCTU': -87276436,
-                             'C0402EBV4': -105936925,
-                             'G1E69LH60': -146980320}
+SLACK_CHANNEL_MATCHING = json.loads(Config.get('Channels', 'Match'))
 
 TELEGRAM_CHANNEL_MATCHING = {tel_channel: slack_channel for slack_channel,
                           tel_channel in SLACK_CHANNEL_MATCHING.items()}
-
 
 SLACK_EMO_MATCHING = {':stuck_out_tongue:': ':P',
                              ':smile:': ':D',
@@ -83,8 +77,8 @@ if __name__ == '__main__':
             message = 'Running Threads: ' + ', '.join(thread.name for
                                                      thread in
                                                      threading.enumerate())
-            slack.post_to_slack(message, 'diagnostics',
-                                     'pats-testing-range')
+            logging.info(message)
+
             time.sleep(60 * 60 * 24)
         except KeyboardInterrupt:
             raise
